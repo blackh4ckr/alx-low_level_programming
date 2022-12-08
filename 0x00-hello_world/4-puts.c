a3a9527ddbc3d9dfb041c5cb98dcaefd314e49ee
blackh4ckr <karorikelvin0@gmail.com>
2022-12-08 08:21:47 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 #include <string.h>
 
 int main(void) {
-    char s[200] = "Programming is like building a multilingual puzzle"
-    puts(s);
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 
 
 }
