a88c4abf45aa84826a6155aaa334c1f6c6e89532
blackh4ckr <karorikelvin0@gmail.com>
2022-12-13 11:07:50 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c, '/n');
+}
