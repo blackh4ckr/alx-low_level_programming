cc3f8e875ea13e111fe3c58a73fd3453fa88a4d4
blackh4ckr <karorikelvin0@gmail.com>
2022-12-13 11:17:40 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
-	_putchar(c, '/n');
+	_putchar(c);
 
 _putchar('/n')
 }
