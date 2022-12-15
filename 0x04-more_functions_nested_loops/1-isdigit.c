696981fa8529ea00dea029aa5d791e73c0a3e4ac
blackh4ckr <karorikelvin0@gmail.com>
2022-12-15 05:33:16 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isdigit - check description
+ * Description: function that checks for a digit (0 through 9).
+ * @c:input
+ * Return: 1 if c is a digit. else returns 0
+ */
+
+int _isdigit(int c)
+{
+if (isdigit(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
