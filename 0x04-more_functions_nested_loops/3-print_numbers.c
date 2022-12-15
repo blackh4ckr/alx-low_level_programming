008e35bfcc997ac6d4792afc6b902ba21c69431d
blackh4ckr <karorikelvin0@gmail.com>
2022-12-15 06:11:34 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - check description
+ * Description: prints numbers 0-9
+ * Return: Always (0)
+ */
+
+void print_numbers(void)
+{
+int i;
+if (i >= '0' && i <= '9')
+{
+_putchar(i, '\n')
+}
+
+return (0);
+}
+
