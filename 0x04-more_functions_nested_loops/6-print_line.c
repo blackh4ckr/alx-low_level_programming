5ab7cd20495c92fe63929cd067d127eb62b321d4
blackh4ckr <karorikelvin0@gmail.com>
2022-12-15 08:32:21 -0800

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - check description
+ * Description: draws a straight line in the terminal
+ * @n:input
+ * Return: 0
+ */
+
+
+void print_line(int n)
+
+{
+
+while (n-- > 0)
+
+{
+
+_putchar('_');
+
+}
+
+_putchar('\n');
+
+}
