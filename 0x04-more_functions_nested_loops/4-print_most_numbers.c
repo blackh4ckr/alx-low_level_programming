1d4132ecfe0eb6e3f07c1b91c98df305c9b77f0b
blackh4ckr <karorikelvin0@gmail.com>
2022-12-15 07:48:36 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,18 +4,19 @@
 /**
  * print_most_numbers - check description
  * Description: print 0-9 except 2 and 4
- * Return: Always 0
+ * Return: Always void
  */
 
 
 void print_most_numbers(void)
 {
-
 int i;
-if ((i != 2) && (i != 4))
+for (i = 0; i < 10; i++)
 {
-_putchar(i);
+if (i != 2 && i != 4)
+{
+_putchar(i + '0')
+}
 }
 _putchar('\n');
-
 }
