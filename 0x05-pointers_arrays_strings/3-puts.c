50b094bc7a450d4563d78060cdb23c4fd76d4710
blackh4ckr <karorikelvin0@gmail.com>
2022-12-19 08:08:09 -0800

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts - check description
+* Description: function that prints a string, followed by a new line
+*@str: string
+*Returns: none
+*/
+
+void _puts(char *str)
+{
+
+_putchar(str);
+_putchar('\n');
+
+}
