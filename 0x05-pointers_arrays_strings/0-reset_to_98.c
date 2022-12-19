74c42b2c63b08b4582a8d5d4beab8fa8f9680a72
blackh4ckr <karorikelvin0@gmail.com>
2022-12-19 06:21:25 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*reset_to_98 - Check description
+*Description: function that takes a pointer updating it to 98
+*@n: pointer
+*Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+
+
+}
