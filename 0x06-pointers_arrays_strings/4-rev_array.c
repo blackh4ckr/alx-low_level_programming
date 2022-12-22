59e726b83d05fb1829d2c05727bb291345dcb1de
blackh4ckr <karorikelvin0@gmail.com>
2022-12-22 01:17:01 -0800

    function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * reverse_array - cehck description
+ * Description: function that reverses the content of an array of integers.
+ * @a: The character to br printed
+ * @n: another
+ * Return: On success 1.
+ * on error, -1 is returned, and errno is set apropriately.
+ */
+
+void reverse_array(int *a, int n)
+{
+int s, u, v;
+s = 0;
+u = 0;
+v[30];
+
+n--;
+while (s <= n)
+{
+v[n - s] = a[s];
+s++;
+}
+while (u <= n)
+{
+a[u] = v[u];
+u++;
+}
+
+
+}
