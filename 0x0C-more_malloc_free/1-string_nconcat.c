f7fa88e4278fde9c5f39b1fefed24f7e86b70acd
blackh4ckr <karorikelvin0@gmail.com>
2023-01-13 10:40:21 -0800

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,61 @@
 #include "main.h"
-#include <stdlib.h>
+
+#include <stdio.h>
+
 #include <string.h>
 
+#include <stdlib.h>
+
 /**
-*string_nconcat - function that concatenates two strings
-*@s1: input
-*@s2: input
-*@n: input
-*Return: NULL if function fails
+* string_nconcat - check description
+* Description: function that concates two strings
+* @s1:string
+* @s2:string2
+* @n: length
+* Return:pointer or NULL if fails
 */
 
 
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+
 {
-int s1_len = strlen(s1);
-int s2_len = strlen(s2);
-char *concatenated_string = malloc(s1_len + n + 1);
+
+char *p;
+
+unsigned int length = n, i;
+
+
 
 if (s1 == NULL)
-{
+
 s1 = "";
-}
+
 if (s2 == NULL)
-{
+
 s2 = "";
-}
 
-if ((int)n >= s2_len)
-{
-n = s2_len;
-}
+for (i = 0; s1[i]; i++)
 
-if (concatenated_string == NULL)
-{
+length++;
+
+
+
+p = malloc(sizeof(char) * (length + 1));
+
+if (p == NULL)
 
 return (NULL);
+length = 0;
+
+for (i = 0; s1[i]; i++)
+
+p[length++] = s1[i];
+
+for (i = 0; s2[i] && i < n; i++)
+
+p[length++] = s2[i];
+p[length] = '\0';
+return (p);
+
 }
-
-
-strcpy(concatenated_string, s1);
-strncat(concatenated_string, s2, n);
-
-return (concatenated_string);
-
-}
-
-
