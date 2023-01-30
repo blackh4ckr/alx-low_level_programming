a9db1fefa6500b3bd913e4268d84f443ba7577c2
blackh4ckr <karorikelvin0@gmail.com>
2023-01-29 23:48:01 -0800

    Function prototypes and data structure

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h> 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -34,3 +38,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+
+#endif
