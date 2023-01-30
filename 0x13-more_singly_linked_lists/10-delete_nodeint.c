5c51279d98c5b1c9bd438bd318d87a6f2fd1e21f
blackh4ckr <karorikelvin0@gmail.com>
2023-01-30 09:27:10 -0800

    Fixing Betty error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -55,13 +55,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	node_to_delete = temp->next;
-
 	temp->next = node_to_delete->next;
-
 	free(node_to_delete);
-
 	return (1);
-
 }
-
-
