f9d61bf1b7dc832720b396e56be32883ff28bcfe
blackh4ckr <karorikelvin0@gmail.com>
2023-01-31 03:43:03 -0800

    function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,124 +1,89 @@
+#include "lists.h"
+
 #include <stdlib.h>
 
 #include <stdio.h>
 
-#include "lists.h"
-
 
 
 /**
- * free_listint_safe - frees a listint_t list
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
  *
- * @h: pointer to the head of the list
+ * @size: size of the new list (always one more than the old list)
+ *
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ *
+ */
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+
+}
+
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ *
+ * @head: double pointer to the start of the list
  *
  * Return: the number of nodes in the list
  *
  */
 
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 
 {
 
-	listint_t *slow = *h, *fast = *h, *temp;
+	size_t i, num = 0;
 
-	int count = 0;
+	listint_t **list = NULL;
 
-
-
-	if (!h || !*h)
-
-		return (0);
+	listint_t *next;
 
 
 
-	while (fast && fast->next)
+	if (head == NULL || *head == NULL)
 
+		return (num);
+
+	while (*head != NULL)
 	{
-
-		count++;
-
-		slow = slow->next;
-
-		fast = fast->next->next;
-
-
-
-		if (fast == slow)
-
+		for (i = 0; i < num; i++)
 		{
-
-			fast = *h;
-
-			count = 0;
-
-			while (fast != slow)
-
+			if (*head == list[i])
 			{
-
-				count++;
-
-				slow = slow->next;
-
-				fast = fast->next;
-
+				*head = NULL;
+				free(list);
+				return (num);
 			}
-
-			count++;
-
-			slow = *h;
-
-			while (fast->next != slow)
-
-			{
-
-				fast = fast->next;
-
-				count++;
-
-			}
-
-			while (*h)
-
-			{
-
-				temp = (*h)->next;
-
-				free(*h);
-
-				*h = temp;
-
-			}
-
-			*h = NULL;
-
-			return (count);
-
 		}
-
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-
-
-	while (*h)
-
-	{
-
-		temp = (*h)->next;
-
-		free(*h);
-
-		*h = temp;
-
-		count++;
-
-	}
-
-	*h = NULL;
-
-
-
-	return (count);
-
+	free(list);
+	return (num);
 }
-
-
