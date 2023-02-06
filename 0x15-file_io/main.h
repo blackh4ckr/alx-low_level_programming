414a234e977e4cd70bd2e30226c3933c4f1aabf4
blackh4ckr <karorikelvin0@gmail.com>
2023-02-06 05:07:04 -0800

    MAIN

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,13 @@
-#ifndef TEXT_FILE_FUNCTIONS_H
-#define TEXT_FILE_FUNCTIONS_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
